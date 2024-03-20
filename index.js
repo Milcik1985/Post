27bbea82e414fe9a48c03d33a69b384181190eac
Milcik1985 <johndoe@example.commilcik@gmail.com>
2024-03-20 10:15:01 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// 1. Parašyt endpointą kuris leistu atspauzdint concolėje filmo rekomendacijos objektą, filmo rekomendacija susideda iš: id, title, raiting, description, imdbLink. Filmo rekomendacija turi būt atsiųsta per body;
+// 2. Pamodifikuot endpointą, sugebėtu išsaugoti filmo rekomendaciją į masyvo kintamąjį; // Reiks rekomandaciją supushint;
+// 3. Parašyt endpointą kuris parsiųstu visas išsaugotas rekomendacijas;
+// 4. Parašyt endpointą kuris gražintu visas rekomendacijas išrikiuotas mažėjimo tvarka pagal reitingą;
+// 5. Parašyt endpointą kuris ištrintų visas rekomendacijas;
+// 6. Patobulint add movie endpointą, kad jus neleistu pridėti filmo su jau egzistuojančiu id;
+// 7. Patobulint savo endpointą bei bei jei masyvas yra tuščias - gražinti 200 statusa su žinute "Data not exist".
+// 8. CAO;
